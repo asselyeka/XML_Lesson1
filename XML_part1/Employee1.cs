963dac37021f05190d378baf6988ac9d4ca275d4
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-09-16 20:08:55 +0600

    Добавьте файлы проекта.

---
+++ XML_part1/Employee1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_part1
+{
+    public class Employee1
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public string Position { get; set; }
+        public int Stage { get; set; }
+        public int Salary { get; set; }
+        public DateTime EmploymentDate { get; set; }
+    }
+}
